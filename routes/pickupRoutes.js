54519a853641be207c27005c2e6c0577c7c6a4fa
DHYAN J PRAKASH <dhyanjprakash@gmail.com>
2025-10-06 11:11:34 +0530

    Delete routes directory

--- routes/pickupRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pickupController = require('../controllers/pickupController');
-
-router.get('/', pickupController.getPickups);
-router.post('/', pickupController.createPickup);
-// Add update, delete routes similarly
-
-module.exports = router;
--- routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-const auth = require('../middleware/auth');
-
-router.get('/users', auth, reportController.userReport);
-
-module.exports = router;
