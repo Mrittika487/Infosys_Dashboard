27802100ef4047ce3abc356ec37583cbcd32cf3d
DHYAN J PRAKASH <dhyanjprakash@gmail.com>
2025-10-06 11:11:22 +0530

    Delete models directory

--- models/Opportunity.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const opportunitySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  status: { type: String, enum: ['open', 'closed'], default: 'open' },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
-module.exports = mongoose.model('Opportunity', opportunitySchema);
